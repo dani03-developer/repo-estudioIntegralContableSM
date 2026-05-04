a0b68f5dfd9c9295cb646c06f3fdfb6b48cb429a
Daniela Machaca <danielamachaca13@gmail.com>
2026-05-04 00:09:01 -0300

    Nav Listo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { BsCart2 } from "react-icons/bs";
+const navigation = [
+  { name: 'Inicio', href: '#', current: true },
+  { name: 'Servicios', href: '#', current: false },
+  { name: 'Nosotros', href: '#', current: false },
+  { name: 'Contacto', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure
+      as="nav"
+      className="relative after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0"
+    >
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group  relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-[#374151] ">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:flex-none sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <img
+                alt="Your Company"
+                src="https://i.postimg.cc/HsBCbZd2/Group-29.png"
+                className="h-8"
+              />
+            </div>
+          </div>
+
+          <div className="hidden sm:absolute sm:left-1/2 sm:-translate-x-1/2 sm:flex">
+            <div className="flex space-x-4">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  aria-current={item.current ? 'page' : undefined}
+                  className={classNames(
+                    item.current ? 'text-white' : 'text-gray-300 hover:bg-white/5 ',
+                    'rounded-md px-3 py-2 text-sm font-medium',
+                  )}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button
+              type="button"
+              className="relative rounded-full p-1 text-white "
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">Ver Carrito</span>
+              <BsCart2 className='size-6'/>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3 bg-[#1e1e1e]">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'text-white' : 'text-white hover:bg-white/5',
+                'block rounded-md px-3 py-2 text-base font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  )
+}
