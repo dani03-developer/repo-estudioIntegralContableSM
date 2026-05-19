4993c9a72cecded058877e51f261f5f439ebb0e8
Daniela Machaca <danielamachaca13@gmail.com>
2026-05-18 22:44:13 -0300

    Modificación de vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/repo-estudioIntegralContableSM/', // ← cambiá esto por el nombre exacto de tu repo
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
