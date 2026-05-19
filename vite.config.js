d7e991bdb1ed198a64ba62b928a64edbdfa54386
Daniela Machaca <danielamachaca13@gmail.com>
2026-05-18 22:52:04 -0300

    Resolucion de problema

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/repo-estudioIntegralContableSM/', // ← cambiá esto por el nombre exacto de tu repo
+  base: '/repo-estudioIntegralContableSM/', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
